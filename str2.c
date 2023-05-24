cdea17a5515d90524494b36eb337c829aeaf9907
btissame <125459632+BTISSAMEHL@users.noreply.github.com>
2023-05-24 22:05:26 +0100

    Update str2.c

--- str2.c
+++ str2.c
@@ -9,9 +9,9 @@
  */
 int _strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index = 0;
+	unsigned int i = 0;
 
-	while ((s1[i] != '\0' || s2[i] != '\0') && index < n)
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
