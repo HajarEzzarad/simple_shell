a23573bc286da959aeb8ca64bbd1841670d1c266
btissame <125459632+BTISSAMEHL@users.noreply.github.com>
2023-07-27 00:53:17 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -94,8 +94,7 @@ typedef struct psinfo
 	int histocount;
 } info_t;
 
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \ 0, 0, 0}
+#define INFO_INIT {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0, 0}
 
 /**
  *struct built - contains a builtin string and related function
